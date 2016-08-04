c6bd41c9ec2db3301991d359f4422f842ad99e58
Jacob Messenger <Jacob Messenger>
2016-08-04 15:06:25 +0100

    Tested and Fixed

--- Web/htbin/index.py
+++ Web/htbin/index.py
@@ -30,7 +30,7 @@ arguments = cgi.FieldStorage()
 if "song" in arguments:
     postQueueToJukebox( arguments['song'])
 MUSIC_FOLDER = "C:\\Users\\gtafi\\Desktop\\pi-juke-\\songs\\"
-TUNES = glob.glob(MUSIC_FOLDER+"*.mp3") +  glob.glob(MUSIC_FOLDER+"*.m4a")
+TUNES = glob.glob(MUSIC_FOLDER+"*.mp4") +   glob.glob(MUSIC_FOLDER+"*.mp3") +  glob.glob(MUSIC_FOLDER+"*.m4a")
 if len(TUNES) == 0:
     print("No Tunes in your JukeBox!")
     print("Exiting....")
@@ -39,9 +39,9 @@ else:
     print (str(len(TUNES)) + " songs found in your JukeBox")
 
 for tune in TUNES :
-    name = nopath( tune )
+    name = noPath( tune )
     noExt = noExtension( name )
-    print( "<a href=\"./index.py/?song=songs/" + name +"\">" + noext +"</a")
+    print( "<a href=\"./index.py/?song=songs/" + name +"\">" + noExt +"</a")
     print( "<h1><font color=\"06dbo6\">" + name + "</font></h1>" )
     print(" <video width=\"240\" height=\"200\" controls>" )
     print("<source src=\"" + tune + "\" type=\"video/mp4\">" )
